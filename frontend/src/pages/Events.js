22a5854364f61924a94a34ff3f530023ae3e28f9
Bander Btati <bander.mohammed.2000@gmail.com>
2026-01-17 09:39:32 +0300

    Error Handling with Custom Errors

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -5,17 +5,24 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+
   const events = data.events;
   return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
+// ملاحظة مهمة: لايمكننا استخدام اي من hooks بداخل loader بسبب ان loader ليست مكوناً من مكونات رياكت
+// ماعادا ذلك يمكننا استخدام اي شيء بداخل loader
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: "Could not fetch events." };
   } else {
     return response;
   }
