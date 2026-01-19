35f98280ff560991b343d0d39461874c621a6dc0
Bander Btati <bander.mohammed.2000@gmail.com>
2026-01-19 10:14:33 +0300

    Submitting Data Programmatically

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { useRouteLoaderData } from "react-router-dom";
+import { useRouteLoaderData, redirect } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
 function EventDetailPage() {
@@ -21,9 +21,28 @@ export async function loader({ request, params }) {
       }),
       {
         status: 500,
-      }
+      },
     );
   } else {
     return response;
   }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({
+        message: "Could not fetch delete event.",
+      }),
+      {
+        status: 500,
+      },
+    );
+  }
+  return redirect("/events");
 }
